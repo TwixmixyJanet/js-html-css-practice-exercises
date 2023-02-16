b73f51d94af6d8ad725e55c39a53302439798459
Janet Webster <twixmixy@gmail.com>
2023-02-16 11:37:48 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var body = document.querySelector("body");
+//console.log(body);
+body.style.backgroundColor = "#d7ddf3";
+
+var header2 = document.querySelector("h2");
+//console.log(header2);
+header2.innerText = "That's Hot!";
+
+var address = document.querySelector("address");
+//console.log(address);
+address.style.fontSize = "1.5em";
+
+var everyDay = document.querySelector("h3");
+//console.log(everyDay);
+everyDay.innerHTML = `We're here for you <em>every day</em> of the week.`;
+//var emphasize = document.querySelector(".emphasize");
+//emphasize.style.textTransform = "uppercase";
+
+var instagram = document.querySelector(".fa-instagram");
+//console.log(instagram);
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+//console.log(facebook);
+facebook.style.color = "#6a99b5";
+
+var hours = document.querySelector(".contact p");
+//console.log(hours);
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1px";
